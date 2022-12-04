85d7aefa55e9e09177811cfa56682e2a52ca5abc
Jordi Been <jordi.been@ridecheck.app>
2022-12-04 13:06:15 +0100

    Day 4 - Part Two

--- src/day4/main.rs
+++ src/day4/main.rs
@@ -5,6 +5,7 @@ fn main() {
     println!("--- Day 4: Camp Cleanup ---");
     let input: String = fs::read_to_string("./src/day4/input.txt").expect("File should exist");
     pt1(&input);
+    pt2(&input);
 }
 
 fn pt1(input: &String) -> () {
@@ -26,11 +27,41 @@ fn pt1(input: &String) -> () {
             pair_set.push(elf_set);
         });
 
-        // If the first set of ID numbers fully contains the other, or the second set of
-        // ID numbers fully contains the first, this line is counted a reconsideration pair
+        // If the first set of ID numbers fully contains the second set, or the second set of
+        // ID numbers fully contains the first set, this line is counted a reconsideration pair
         if &pair_set[0].is_subset(&pair_set[1]) | &pair_set[1].is_subset(&pair_set[0]) {
             count += 1
         };
     });
     println!("The answer for Part One is: {}", count);
+}
+
+fn pt2(input: &String) -> () {
+    // Start Part Two
+    println!("Part Two");
+
+    let mut count: u32 = 0;
+    input.lines().for_each(|line| {
+        // Save elf pairs to a Vec of HashSets containing their assigned ID numbers
+        // Ex. [{2, 3, 4}, {5, 6, 7}]
+        let mut pair_set: Vec<HashSet<u32>> = vec![];
+        line.split(",").for_each(|elf_range| {
+            let range: Vec<u32> = elf_range
+                .split("-")
+                .map(|val| val.parse::<u32>().expect("Range should be number"))
+                .collect();
+
+            let elf_set: HashSet<u32> = (range[0]..range[1] + 1).collect();
+            pair_set.push(elf_set);
+        });
+
+        // If the first set of ID numbers intersects with the second set, this
+        // line is counted an overlapping pair
+        let intersection: HashSet<_> = pair_set[0].intersection(&pair_set[1]).collect();
+        if intersection.len() > 0 {
+            count += 1
+        }
+    });
+
+    println!("The answer for Part Two is: {}", count);
 }
