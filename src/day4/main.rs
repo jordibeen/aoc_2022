afd713d95e841e6227abaa2028b9f0bb8beccc98
Jordi Been <jordi.been@ridecheck.app>
2022-12-04 12:40:37 +0100

    Day 4 - Part One

---
+++ src/day4/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn main() {
+    println!("--- Day 4: Camp Cleanup ---");
+    let input: String = fs::read_to_string("./src/day4/input.txt").expect("File should exist");
+    pt1(&input);
+}
+
+fn pt1(input: &String) -> () {
+    // Start Part One
+    println!("Part One");
+
+    let mut count: u32 = 0;
+    input.lines().for_each(|line| {
+        // Save elf pairs to a Vec of HashSets containing their assigned ID numbers
+        // Ex. [{2, 3, 4}, {5, 6, 7}]
+        let mut pair_set: Vec<HashSet<u32>> = vec![];
+        line.split(",").for_each(|elf_range| {
+            let range: Vec<u32> = elf_range
+                .split("-")
+                .map(|val| val.parse::<u32>().expect("Range should be number"))
+                .collect();
+
+            let elf_set: HashSet<u32> = (range[0]..range[1] + 1).collect();
+            pair_set.push(elf_set);
+        });
+
+        // If the first set of ID numbers fully contains the other, or the second set of
+        // ID numbers fully contains the first, this line is counted a reconsideration pair
+        if &pair_set[0].is_subset(&pair_set[1]) | &pair_set[1].is_subset(&pair_set[0]) {
+            count += 1
+        };
+    });
+    println!("The answer for Part One is: {}", count);
+}
